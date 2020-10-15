01241aba9f76b30b800a09cd1ed9dd360914ea5c
chiragcharania <chiragcharania@gmail.com>
2020-10-15 16:33:30 -0400

    Updates

---
+++ precache-manifest.11f3cb444c888611f44b757ec9962c10.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d443ef8abe1b52f6a0f5315645e2781f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "df1d0d135bf01211c729",
+    "url": "/portfolio/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "4f0adf2dd3939927f009",
+    "url": "/portfolio/static/css/main.973d2b05.chunk.css"
+  },
+  {
+    "revision": "df1d0d135bf01211c729",
+    "url": "/portfolio/static/js/2.2f495346.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.2f495346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0adf2dd3939927f009",
+    "url": "/portfolio/static/js/main.fdf7b695.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9ea501a3ceb090f552eaf92c72fba4a4",
+    "url": "/portfolio/static/media/github.9ea501a3.jpg"
+  },
+  {
+    "revision": "61a510abe7ec8bae220c0e0e5b8d9ea1",
+    "url": "/portfolio/static/media/harts.61a510ab.jpg"
+  },
+  {
+    "revision": "8ef3983f24e1d5fba74df65e222f1374",
+    "url": "/portfolio/static/media/mindmap.8ef3983f.jpg"
+  }
+]);
