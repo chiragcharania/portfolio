2b9d8621d98407e9706acb6e25611f943a912eff
chiragcharania <chiragcharania@gmail.com>
2020-10-15 16:47:08 -0400

    deletion change

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,6 @@ function Card(props) {
             <img className="c-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
             { props.item.selected && < CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />}
         </div >
-
     );
 }
 
